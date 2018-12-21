ea3e277b39013e022525dfee3be4e1ef99340b16
明宇 葛 <g.m.y@live.cn>
2018-12-21 21:42:19 +0800

    complete

---
+++ Simple HTTP Server/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace Simple_HTTP_Server
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(146, 40);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "开启监听";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(12, 72);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(131, 19);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "验证完成后退出";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.MenuText;
+            this.textBox1.Font = new System.Drawing.Font("微软雅黑", 9.07563F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox1.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.textBox1.Location = new System.Drawing.Point(12, 97);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(303, 145);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "就绪";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(170, 13);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(146, 40);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "复制验证码";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(327, 254);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "验证服务器";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Simple HTTP Server/Form1.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Collections;
+
+
+namespace Simple_HTTP_Server
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+           
+
+        }
+
+        //判断HOSTS是否包含str
+        private static bool checkHosts(string filename,string str)
+        {
+            StreamReader sr = new StreamReader(filename);
+            string allText = sr.ReadToEnd();
+            sr.Close();
+           return allText.Contains(str);
+            
+        }
+
+
+        /// <summary>
+        /// 修改hosts文件
+        /// </summary>
+        private static void updateHosts(string ip, string host)
+        {
+            string path = @"C:\WINDOWS\system32\drivers\etc\hosts";
+            //通常情况下这个文件是只读的，所以写入之前要取消只读
+            File.SetAttributes(path, File.GetAttributes(path) & (~FileAttributes.ReadOnly));//取消只读
+            //1.创建文件流
+            FileStream fs = new FileStream(path, FileMode.Append);
+            //2.创建写入器
+            StreamWriter sw = new StreamWriter(fs, Encoding.UTF8);
+            //3.开始写入
+            bool result = false;//标识是否写入成功
+            try
+            {
+                StringBuilder sb = new StringBuilder();
+                //追加模式写入
+                sb.Append(ip);//IP地址
+                sb.Append("   ");
+                sb.Append(host);//网址
+                sw.WriteLine(sb.ToString());
+                result = true;
+            }
+            catch (Exception ex)
+            {
+                result = false;
+            }
+            finally
+            {
+                //4.关闭写入器
+                if (sw != null)
+                {
+                    sw.Close();
+                }
+                //5.关闭文件流
+                if (fs != null)
+                {
+                    fs.Close();
+                }
+            }
+            if (result == true)
+            {
+                //MessageBox.Show("写入成功！");
+                File.SetAttributes(path, File.GetAttributes(path) | FileAttributes.ReadOnly);//设置只读
+            }
+            else
+            {
+                //MessageBox.Show("写入失败！");
+                return;
+            }
+        }
+
+
+
+        /// <summary>
+        /// //http服务器部分
+        /// </summary>
+        //定义是否执行完技术标志
+        bool isActive = true;
+
+        public void Listen(string address)
+        {
+            
+
+            if (!HttpListener.IsSupported)
+                throw new InvalidOperationException(
+                    "请确保使用WindowsXP以上版本的Windows!");
+
+            //初始化Http监听器
+            HttpListener listener = new HttpListener();
+
+            //初始化服务器URL
+            //string[] prefixes = new string[] { address };
+            // foreach (string prefix in prefixes)
+            //{
+            //   listener.Prefixes.Add(prefix);
+            // }
+            listener.Prefixes.Add(address);
+
+
+
+            //创建新[线程开始代码]循环监听
+           ;
+            ThreadStart Ths = new ThreadStart(delegate
+            {
+
+
+                do//循环开关服务器接受请求
+                {
+                    //开启服务器
+                    listener.Start();
+                    HttpListenerContext context = listener.GetContext();
+                    HttpListenerRequest request = context.Request;
+                    HttpListenerResponse response = context.Response;
+
+                    if (request.HttpMethod == "GET")
+                    {
+                        textBox1.AppendText("\r\n接收到GET请求");
+                        OnGetRequest(request, response);
+
+                    }
+                    else
+                    {
+                        textBox1.AppendText("\r\n接收到POST请求");
+                        OnPostRequest(request, response);
+
+                    }
+                    //关闭服务
+                    listener.Stop();
+                    
+                }
+                while (isActive);//isActive时不退出循环继续接受 
+            });
+
+            Thread Th = new Thread(Ths);//创建新线程
+            Th.Start();//开启线程
+
+        }
+
+        private void OnPostRequest(HttpListenerRequest request, HttpListenerResponse response)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void OnGetRequest(HttpListenerRequest request, HttpListenerResponse response)
+        {
+            //定义返回字符串
+            string responseString = "Your product is activated";
+
+            //定义响应输出流
+            Stream output = response.OutputStream;
+
+            //将字符串编码成字节
+            byte[] buffer = Encoding.UTF8.GetBytes(responseString);
+
+            //获取字节长度
+            response.ContentLength64 = buffer.Length;
+
+            //将字节和字节长度写入响应输出流
+            output.Write(buffer, 0, buffer.Length);
+
+            //关闭
+            output.Close();
+
+            textBox1.AppendText("\r\n成功");
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //如果hosts里面不存在相应hosts则添加
+            if (!checkHosts(@"C:\WINDOWS\system32\drivers\etc\hosts", "www.example.com")) {
+                textBox1.AppendText("\r\n正在添加HOSTS...");
+                updateHosts("127.0.0.1", "www.pubmedchina.com");
+            }
+            else
+            {
+                textBox1.AppendText("\r\nHOSTS已存在...");
+            }
+            
+            if (button1.Text == "开启监听")//开启监听
+            {
+                textBox1.AppendText("\r\n监听服务开启,等待验证请求...");
+
+                if (checkBox1.Checked==true)  {
+                    
+                    isActive = true;                   
+                }
+                else
+                {
+                    isActive = false;               
+                }
+
+                Listen("http://127.0.0.1:80/");
+
+                button1.Text = "关闭监听并退出";//切换按钮文字
+            }
+
+            else
+            {
+                textBox1.AppendText("\r\n关闭线程并退出...");
+                isActive = false;
+                Application.ExitThread();
+            }
+
+            
+
+            
+
+            
+        }
+
+        
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            int code = random.Next(147100000, 147189999);
+            Clipboard.SetText(code.ToString());
+            textBox1.AppendText("\r\n验证码复制成功:" + code);
+        }
+    }
+}
